0f50bf174d4525e8058192fc0f1d47a85ba3f794
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-04-14 01:23:21 +0300

    Scanner.cpp resolveHost function

--- port_scanner/scanner.cpp
+++ port_scanner/scanner.cpp
@@ -19,3 +19,18 @@
 #include <sstream>
 
 #pragma comment(lib, "ws2_32.lib")
+
+// Helper functions
+static bool resolveHost(const std::string& host, sockaddr_in& addr) {
+    addrinfo hints{};
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    addrinfo* res = nullptr;
+
+    if (getaddrinfo(host.c_str(), nullptr, &hints, &res) != 0 || !res)
+        return false;
+
+    addr = *reinterpret_cast<sockaddr_in*>(res->ai_addr);
+    freeaddrinfo(res);
+    return true;
+}
